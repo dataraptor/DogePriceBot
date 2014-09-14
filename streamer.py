b229c0ec269a64922753866dde9f33a5e2d7b856
Ryan Cunningham <rydcunningham@gmail.com>
2014-09-14 13:03:38 -0500

    Updated streamer to print price changes

--- streamer.py
+++ streamer.py
@@ -8,10 +8,10 @@ from dbwrapper import Wrapper
 
 class Streamer:
 	#Global variables
-	consumer_key = 'Mhy5lNERB3dTkT3wcWeFGw'
-	consumer_secret = 'ozX3svU54uif0bZWn1jt0DrQwSmHoAWnh0ZToBYVFI'
-	access_token = '2409405422-JOZnjcCh4ZiMngnT6x0tEAKRSf9iq8s6nPZoDyr'
-	access_token_secret = 'o3xl4L4WTIFZGlAjUmlylClAVNNJf49OyvCuhdtnsvt83'
+	consumer_key = ''
+	consumer_secret = ''
+	access_token = ''
+	access_token_secret = ''
 
 	# OAuth process, using the keys and tokens
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -28,9 +28,20 @@ class Streamer:
 		timestamp = datetime.fromtimestamp(time.time())
 		rates = self.assembler.assemble(base, mid, quote)
 		base_per_mid, mid_per_quote = rates[0], rates[1]
+		last_update = self.wrapper.c.execute("SELECT * FROM Prices ORDER BY month DESC, day DESC, hour DESC").fetchone()
+		new_price = base_per_mid*mid_per_quote
+		last_price = last_update[5]*last_update[6]
+		delta = new_price/last_price - 1
 		self.wrapper.update_price_db(timestamp, base_per_mid, mid_per_quote)
-		return '[%s CST]: The average dogecoin price is now %.2f bits ($%.6f) #dogepricebottest' \
-		% (timestamp.strftime('%m-%d %H:%M'), base_per_mid*(1000000), base_per_mid*mid_per_quote)
+		if delta >= 0:
+			return '[%s CST]: The average #dogecoin price is now $%.6f, +%.1f%% growth wow (%.2f bits)' \
+			% (timestamp.strftime('%m-%d %H:%M'), new_price, delta*100, base_per_mid*(1000000))
+		else:
+			return '[%s CST]: The average #dogecoin price is now $%.6f, +%.1f%% decline (%.2f bits)' \
+			% (timestamp.strftime('%m-%d %H:%M'), new_price, delta*100, base_per_mid*(1000000))	
+		
+		#return '[%s CST]: The average #dogecoin price is now $%.6f (%.2f bits)' \
+		#% (timestamp.strftime('%m-%d %H:%M'), new_price, base_per_mid*(1000000))
 		#return '[%s CST]: The average dogecoin price is now %.2f bits ($%.6f).\n$1 = Ð%.2f\n1BTC = Ð%d' % (datetime.fromtimestamp(time.time()).strftime('%m-%d %H:%M:%S'), dogebtcprice, dogeusdprice, 1/dogeusdprice, dogebtcprice*100000)
 
 	#Continuous price stream
@@ -39,9 +50,9 @@ class Streamer:
 		while True:
 			try:
 				tweet = self.priceupdate('DOGE', 'BTC', 'USD')
-#				self.api.update_status(tweet)
-				print tweet
-				print 'Tweeted successfully'
+				self.api.update_status(tweet)
+				#print tweet
+				#print 'Tweeted successfully'
 			except Exception, e:
 				print str(e)
 			time.sleep(3570)
